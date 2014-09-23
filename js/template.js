05b817b9200a1c2c0a4a20601fa69c765ef858cc
bobfraser1 <bobfraser1@gmail.com>
2014-09-23 11:42:36 -0700

    CSS updates

--- js/template.js
+++ js/template.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$.get('templates/header.html', function(data) {
-		$('#content').before(data);
+		$('#page').prepend(data);
 	});
 	$.get('templates/footer.html', function(data) {
-		$('#content').after(data);
+		$('#page').append(data);
 	});
 });
